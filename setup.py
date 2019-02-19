220eb3ef0ff3dfc889c6cf83b97fda95710023dd
pianobacki <backhaus.info@googlemail.com>
2019-02-19 16:51:46 +0100

    package install stuff

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ import setuptools
 long_description = open("README.md").read()
 
 setuptools.setup(
-    name="minima-test-zbyszek",
-    version="0.0.1",
-    author="Zbigniew Jędrzejewski-Szmek",
-    author_email="zbyszek@in.waw.pl",
+    name="minima-test-test-schwdback",
+    version="0.0.2",
+    author="Lisback- Danlick",
+    author_email="wasnice@uni-potsdam.de",
     description="A library to find function maxima",
 
     long_description=long_description,
     long_description_content_type="text/markdown",
 
-    url="https://github.com/potsdam-python-workshop/testing",
+    url="https://github.com/pianobacki/testing.git",
     packages=['maxima'],
 
     classifiers=[
