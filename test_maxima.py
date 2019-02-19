587a40e4f86b4fb169c7e6135a9104b5c50367f5
pianobacki <backhaus.info@googlemail.com>
2019-02-19 11:37:04 +0100

    fuzzing test

--- test_maxima.py
+++ test_maxima.py
@@ -2,51 +2,13 @@ import numpy as np
 import pytest
 from maxima import find_maxima
 
-#def test_simple_sequence_two_maxima():
-#    inp = [0, 1, 2, 1, 2, 1, 0]
-#    out = find_maxima(inp)
-#    exp = [2, 4]
-#    assert exp == out
-#
-#def test_simple_sequence_one_maximum():
-#    inp = [-i**2 for i in range(-3, 4)]
-#    out = find_maxima(inp)
-#    exp = [3]
-#    assert exp == out
-#
-#def test_sine_wave():
-#    inp = [np.sin(2*alpha) for alpha in np.linspace(0.0, 5.0, 100)]
-#    out = find_maxima(inp)
-#    exp = [16,78]
-#    assert exp == out
+seed = 5
+rand_state = np.random.RandomState(seed)
 
-
-test_cases = [([0, 1, 2, 1, 2, 1, 0],[2, 4]),
-               ([-i**2 for i in range(-3, 4)],[3]),
-               ([np.sin(2*alpha) for alpha in np.linspace(0.0, 5.0, 100)], [16,78])]
-
-@pytest.mark.parametrize('input, expected_result', test_cases)
-def test_maxima(input,expected_result):
-    #Given: input
-    #When
-    output = find_maxima(input)
-    #Then
-    assert output == expected_result
-
-
-
-
-# additional tests for
-# - max on both borders
-#   x = [4, 2, 1, 3, 1, 2]
-# - max on both borders, absolute max on right border
-#   x = [4, 2, 1, 3, 1, 5]
-# - one max (absolute) on left border
-#   x = [4, 2, 1, 3, 1]
-# - plateau
-#   x = [1, 2, 2, 1]
-#   (decide for a sensible output in this case)
-# - test cases for plateau
-#   x = [1, 2, 2, 3, 1]
-#   x = [1, 3, 2, 2, 1]
-#   x = [3, 2, 2, 3]
+def test_maxima():
+    testarr = np.random.randint(5, size = 10)
+    testarr[3] = 10
+    output = find_maxima(testarr)
+    expected_result = 3
+    assert expected_result in output
+    
